e8a673990bb8c398885d74bd8c48602686412a7a
Jason Andrada <jpa323@nyu.edu>
2019-10-28 16:30:00 -0400

    implement promisePending for todo state management

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,10 +1,12 @@
 import {
   QUERY_TODOS_LIST,
   TODO_QUERY_REJECTED,
-  TODO_QUERY_RESOLVED 
+  TODO_QUERY_RESOLVED,
+  PROMISE_PENDING
 } from '../constants/types';
 
 const initialState = {
+  promisePending: false,
   todoQueryResult: null,
   todoErr: null
 }
@@ -13,14 +15,21 @@ export const todo = (state = initialState, action) => {
   switch(action.type){
     case QUERY_TODOS_LIST:
       return state
+    case PROMISE_PENDING:
+      return {
+        ...state,
+        promisePending: true
+      }
     case TODO_QUERY_RESOLVED:
       return {
         ...state,
+        promisePending: false,
         todoQueryResult: action.res
       }
     case TODO_QUERY_REJECTED:
       return {
         ...state,
+        promisePending: false,
         todoErr: action.err
       }
     default:
